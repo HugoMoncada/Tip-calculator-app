1233b4c643bf5b3d1cf2384f238b59bb432e6756
Hugo Moncada <hugo_moncada@outlook.com>
2022-07-05 12:04:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Frontend Mentor Responsive HTML-CSS-JS - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size - status = ✔
+- See hover states for all interactive elements on the page - status = ✔
+- Calculate the correct tip and total cost of the bill per person - status = ✔
+
+### Screenshot
+
+![](./images/Mobile.png)
+![](./images/Desktop.png)
+
+
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@HugoMoncada](https://www.frontendmentor.io/profile/HugoMoncada)
+
---
+++ js/app.js
@@ -0,0 +1,60 @@
+let bill = document.getElementById("bill");
+let people = document.getElementById("people");
+let total_amount = document.querySelector("#total_amount"); 
+let totalTip = document.querySelector("#total_tip");
+let percetageButtons = document.querySelectorAll("input[name='tip']")
+let customTip = document.getElementById("customTip");
+let resetBtn = document.getElementById("reset");
+let totalObj = {
+    bill: "", 
+    people: ""
+}
+
+reset();
+
+percetageButtons.forEach( button => {
+    button.addEventListener("click", () => {
+        customTip.value = ""; 
+        calculate()
+    })
+})
+
+bill.addEventListener("input", () => {
+    totalObj.bill = bill.value;
+    calculate();
+});
+
+people.addEventListener("input", () => {
+    totalObj.people = people.value;
+    calculate()
+});
+
+customTip.addEventListener("input", () => {
+    calculate();
+});
+
+resetBtn.addEventListener("click", reset);
+
+function calculate(){
+    if(totalObj.bill != "" && totalObj.people != ""){
+        let selectedPercentage = document.querySelector("input[name='tip']:checked").value;
+        if(customTip.value != ""){
+            selectedPercentage = Number(customTip.value);
+        }
+        let tipValue = Number((totalObj.bill  * (selectedPercentage / 100 ) ) / totalObj.people).toFixed(2);       
+        totalTip.innerHTML = tipValue;
+        total_amount.innerHTML = Number(totalObj.bill / totalObj.people + Number(tipValue)).toFixed(2); 
+    }
+}
+
+function reset(){
+    totalObj = {
+        bill: "", 
+        people: ""
+    }
+    total_amount.innerHTML = 0;
+    totalTip.innerHTML = 0;
+    bill.value = "";
+    people.value = "";
+    customTip.value = ""; 
+}
